e6c108082f7b481df393fd5c7520ab4eb21a78bc
erik <erik@iotech.systems>
2023-08-13 14:26:55 +0200

    init load

---
+++ app_xora/xoraWeb/www/js/cam-top.js
@@ -0,0 +1,26 @@
+
+class CamTop {
+
+   static tickCount = 0;
+
+   constructor() {
+      this.tickInterval = 200;
+      this.timerInt = 0;
+      this.lastTick;
+      this.msg;
+   }
+
+   init() {
+      this.timerInt = setInterval(this.camTick, this.tickInterval);
+   }
+
+   run() {
+     console.log("camtop run ...");
+   }
+
+   camTick() {
+      CamTop.tickCount++;
+      console.log(`cam_tick: ${CamTop.tickCount}`);
+   }
+
+};
