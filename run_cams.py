8bad0f66207ba9133645a1c3115afc5177347e0e
erik <erik@iotech.systems>
2023-08-15 23:22:38 +0200

    path fix

---
+++ run_cams.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import time
+from apps.xoraCams.cams.topCam import topCam
+
+
+MAIN_TICK_SLEEP: float = 4.0
+TOP_CAM: topCam = topCam()
+
+
+def main():
+   def __main_tick():
+      pass
+   # -- main ticks --
+   while True:
+      __main_tick()
+      time.sleep(MAIN_TICK_SLEEP)
+
+
+# -- -- -- entry point -- -- --
+if __name__ == "__main__":
+   main()
