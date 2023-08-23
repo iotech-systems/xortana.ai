ca5cf56649c2071a689af14722afd229f38c3096
erik <erik@iotech.systems>
2023-08-23 18:25:27 +0200

    live view

--- shared/sysOps.py
+++ shared/sysOps.py
@@ -11,7 +11,7 @@ class sysOps(object):
          if not os.path.exists(fpath):
             return 1, None
          fls = os.listdir(fpath)
-         ffps = [(fn, os.stat(f"{fpath}/fn")) for fn in fls]
+         ffps = [(fn, os.stat(f"{fpath}/{fn}")) for fn in fls]
          print(ffps)
          return 0, fls
       except Exception as e:
