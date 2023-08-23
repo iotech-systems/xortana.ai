1e4e64cc9d5ef53f87dd25fa56e2144190b26531
erik <erik@iotech.systems>
2023-08-23 18:30:42 +0200

    live view

--- shared/sysOps.py
+++ shared/sysOps.py
@@ -1,5 +1,6 @@
 
 import os
+from stat import S_ISREG, ST_MODE, ST_CTIME
 
 
 class sysOps(object):
@@ -11,7 +12,7 @@ class sysOps(object):
          if not os.path.exists(fpath):
             return 1, None
          fls = os.listdir(fpath)
-         ffps = [(fn, os.stat(f"{fpath}/{fn}")) for fn in fls]
+         ffps = [(fn, os.stat(f"{fpath}/{fn}")[ST_CTIME]) for fn in fls]
          print(ffps)
          return 0, fls
       except Exception as e:
