c24c4b6008611566adb63bde6501f2d9b600db54
erik <erik@iotech.systems>
2023-08-23 18:49:31 +0200

    live view

--- shared/sysOps.py
+++ shared/sysOps.py
@@ -9,17 +9,20 @@ class sysOps(object):
    def folder_files(fpath: str) -> (int, []):
       #  -- -- -- --
       def _sort_func(i):
-         print(i)
-         return i[0]
+         return i[1]
       #  -- -- -- --
       try:
          print(f"folder_files: {fpath}")
          if not os.path.exists(fpath):
             return 1, None
+         # -- -- -- --
          fls = os.listdir(fpath)
          ffps = [(fn, os.stat(f"{fpath}/{fn}")[ST_CTIME]) for fn in fls]
          print(ffps)
-         ffps_sorted = ffps.sort(key=_sort_func)
+         # ffps_sorted = ffps.sort(key=_sort_func, reverse=True)
+         ffps_sorted = sorted(ffps, key=_sort_func, reverse=True)
+         print(ffps_sorted)
+         # -- -- -- --
          return 0, ffps_sorted
       except Exception as e:
          return 2, f"{e}"
