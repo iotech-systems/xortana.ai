78fe19562ab30b290ed28735f1ea5f88eaed12c2
erik <erik@iotech.systems>
2023-08-23 18:38:50 +0200

    live view

--- shared/sysOps.py
+++ shared/sysOps.py
@@ -17,6 +17,7 @@ class sysOps(object):
             return 1, None
          fls = os.listdir(fpath)
          ffps = [(fn, os.stat(f"{fpath}/{fn}")[ST_CTIME]) for fn in fls]
+         print(ffps)
          ffps_sorted = ffps.sort(key=_sort_func)
          return 0, ffps_sorted
       except Exception as e:
