906a9726c97421d0302bc18c370153c5668a58c5
erik <erik@iotech.systems>
2023-08-23 19:51:26 +0200

    live view

--- shared/sysOps.py
+++ shared/sysOps.py
@@ -8,8 +8,8 @@ class sysOps(object):
    @staticmethod
    def folder_files(fpath: str) -> (int, []):
       #  -- -- -- --
-      def _sort_func(i):
-         return i[1]
+      def _sort_func(i) -> int:
+         return int(i[1])
       #  -- -- -- --
       try:
          print(f"folder_files: {fpath}")
@@ -18,10 +18,7 @@ class sysOps(object):
          # -- -- -- --
          fls = os.listdir(fpath)
          ffps = [(fn, os.stat(f"{fpath}/{fn}")[ST_CTIME]) for fn in fls]
-         print(ffps)
-         # ffps_sorted = ffps.sort(key=_sort_func, reverse=True)
          ffps_sorted = sorted(ffps, key=_sort_func, reverse=True)
-         print(ffps_sorted)
          # -- -- -- --
          return 0, ffps_sorted
       except Exception as e:
