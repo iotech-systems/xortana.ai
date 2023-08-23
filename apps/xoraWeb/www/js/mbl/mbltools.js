bd3bc1ddd5be66a9070fb9844402b1638f9c6098
erik <erik@iotech.systems>
2023-08-24 01:58:34 +0200

    btn fix

--- apps/xoraWeb/www/js/mbl/mbltools.js
+++ apps/xoraWeb/www/js/mbl/mbltools.js
@@ -6,12 +6,20 @@ var mbltools = {
       },
 
       skyCamTakePic() {
-         /* "/cam/<camid>/<act>/<args>" */
+         /* -- */
+         $("div#btnTopCamTakePic").css("opacity", "0.5");
+         $("div#btnTopCamTakePic").off();
          let img_prefix = $("#txtImgPrefix").val(), 
             url = `/cam/sky/take_img/(${img_prefix})`;
+         /* -- */
          $.post(url, (resp) => {
                console.log(resp);
             });
+         /* -- */
+         setInterval(() => {
+               $("div#btnTopCamTakePic").css("opacity", "1.0");
+               $("div#btnTopCamTakePic").off().on("click", mbltools.skyCamTakePic);
+            }, 2400);
       }
 
 };
