238f4b63f3aea5e48d7e2df019ec85ffb9d856f2
erik <erik@iotech.systems>
2023-08-23 14:54:01 +0200

    live view

--- apps/xoraWeb/www/js/liveview.js
+++ apps/xoraWeb/www/js/liveview.js
@@ -11,9 +11,33 @@ class liveViewCls {
    }
 
    peekFolder(fld) {
-      let url = `/view/tf/${fld}`
-      $.get(url, (jsObj) => {
-            console.log(jsObj);
+      let _this = this, 
+         url = `/view/tf/${fld}`
+      /* -- -- */
+      $.get(url, function(jsArr) {
+            $("div#viewPort").html("");
+            _this.showThums(jsArr);
+            /* -- -- */
+            jsArr.forEach((i) => {
+                  $.get(`/load/tf/img/thums/${i}`, (b64) => {
+                        let src = `data:image/jpg;base64, ${b64}`,
+                           dhtml = `<div><img src="${src}"></div>`;
+                        $("div#viewPort").html(dhtml);
+                     });
+               });
+            /* -- -- */
+         });
+      /* -- -- */
+   }
+
+   /* <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
+      AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
+         9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" /> */
+   showThums(arr) {
+      arr.forEach((i) => {
+            let src = "/", 
+               dhtml = `<div><img src="${src}"></div>`;
+            $("div#viewPort").html(dhtml);   
          });
    }
 
