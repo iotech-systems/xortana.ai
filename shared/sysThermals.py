6116a75c7d22bb6275e9b8926e874b2d4ed64695
erik <erik@iotech.systems>
2023-08-24 02:46:17 +0200

    adding thermals

--- shared/sysThermals.py
+++ shared/sysThermals.py
@@ -8,11 +8,23 @@ class sysThermals(object):
 
    RIGHT_I2C_ADDR: int = 0x68
    LEFT_I2C_ADDR: int = 0x69
+   RIGHT_I2C_READ: bool = True
+   LEFT_I2C_READ: bool = True
 
    def __init__(self):
       self.i2c: busio.I2C = busio.I2C(board.SCL, board.SDA)
-      self.right_amg8833 = adafruit_amg88xx.AMG88XX(self.i2c, sysThermals.RIGHT_I2C_ADDR)
-      self.left_amg8833 = adafruit_amg88xx.AMG88XX(self.i2c, sysThermals.LEFT_I2C_ADDR);
+      try:
+         self.right_amg8833 = (
+            adafruit_amg88xx.AMG88XX(self.i2c, sysThermals.RIGHT_I2C_ADDR))
+      except Exception as e:
+         sysThermals.RIGHT_I2C_READ = False
+         print(e)
+      try:
+         self.left_amg8833 = (
+            adafruit_amg88xx.AMG88XX(self.i2c, sysThermals.LEFT_I2C_ADDR))
+      except Exception as e:
+         sysThermals.LEFT_I2C_ADDR = False
+         print(e)
       self.amg8833_thread = threading.Thread(target=self.__amg8833_thread)
 
    def init(self):
@@ -20,8 +32,10 @@ class sysThermals(object):
 
    def __amg8833_thread(self):
       while True:
-         self.__amg883_read_left()
-         self.__amg883_read_right()
+         if sysThermals.LEFT_I2C_READ:
+            self.__amg883_read_left()
+         if sysThermals.RIGHT_I2C_READ:
+            self.__amg883_read_right()
          time.sleep(0.2)
 
    def __amg883_read_left(self):
