4aeb9b0e4aecedea51dd34071bf0c7e95c746058
erik <erik@iotech.systems>
2023-08-24 21:44:10 +0200

    added min max

--- apps/xoraWeb/www/js/amg8833s.js
+++ apps/xoraWeb/www/js/amg8833s.js
@@ -6,8 +6,8 @@ class amg8833Sensor {
    tickMS = 200;
 
    constructor(canvasID, minTemp = 20, maxTemp = 40) {
-      this.convasID = canvasID;
-      this.grid = new amg8833Grid(this.canvasID);
+      this.canvasID = canvasID;
+      this.grid = new amg8833Grid(this.canvasID, 8, 8, minTemp, maxTemp);
       this.grid.init();
    }
 
@@ -24,14 +24,17 @@ class amg8833Sensor {
 class amg8833Grid {
 
    constructor(canvasID, cols = 8, rows = 8, minTemp = 20, maxTemp = 40) {
+      /* -- */
       this.canvasID = canvasID;
       this.cols = cols;
       this.rows = rows;
       this.minTemp = minTemp;
       this.maxTemp = maxTemp;
+      /* -- */
       this.canvas = document.getElementById(this.canvasID);
       if (this.canvas == null)
          console.log(`CanvasIsNull: ${this.canvasID}`);
+      /* -- */
       this.cntx2d = this.canvas.getContext("2d");
       this.css_width = 0;
       this.css_height = 0;
