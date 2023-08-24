7ba10b83006461f62277ef3347b074ec88a47853
erik <erik@iotech.systems>
2023-08-24 18:31:11 +0200

    adding thermals

--- apps/xoraWeb/www/js/tfViewCls.js
+++ apps/xoraWeb/www/js/tfViewCls.js
@@ -3,7 +3,7 @@ var liveView = null;
 var TICK_INTERVAL = 16000;
 
 
-class liveViewCls {
+class tfViewCls {
 
    static Instance = null;
 
@@ -15,7 +15,7 @@ class liveViewCls {
       /* -- */
       this.last_dirlst = [];
       $("div#viewPort").html("");
-      liveViewCls.Instance = this;
+      tfViewCls.Instance = this;
       /* -- */
    }
 
@@ -27,7 +27,7 @@ class liveViewCls {
       /* -- */
       let url = `/view/tf/${fld}`;
       let _on_get = function(jsArr) {
-            jsArr.forEach(liveViewCls.Instance.onArrItem);
+            jsArr.forEach(tfViewCls.Instance.onArrItem);
             let dts = new Date().toLocaleString();
             $("div#lastTickDts").html(dts);
          };
@@ -38,10 +38,10 @@ class liveViewCls {
    onArrItem(i) {
       /* -- */
       let fn = String(i[0]);
-      if (liveViewCls.Instance.last_dirlst.includes(fn))
+      if (tfViewCls.Instance.last_dirlst.includes(fn))
          return;
       /* -- */
-      liveViewCls.Instance.last_dirlst.push(fn);
+      tfViewCls.Instance.last_dirlst.push(fn);
       $.get(`/load/tf/img/thums/${fn}`, (b64) => {
             let src = `data:image/jpg;base64, ${b64}`,
                dhtml = `<div class="thum-box" fn="${fn}"><img src="${src}"/>` + 
@@ -70,6 +70,6 @@ class liveViewCls {
 
 /* -- -- [ on doc loaded ] -- -- */
 document.addEventListener("DOMContentLoaded", () => {
-      liveView = new liveViewCls();
+      liveView = new tfViewCls();
       liveView.init();
    });
