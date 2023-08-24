9ffde61a027fc6fb8c9af661abdb0260b10fcd8f
erik <erik@iotech.systems>
2023-08-24 19:27:42 +0200

    adding thermals

--- apps/xoraWeb/www/js/amg8833s.js
+++ apps/xoraWeb/www/js/amg8833s.js
@@ -13,6 +13,31 @@ class amg8833s {
 
    }
 
+   tempToColor(temp) {
+      return ""
+   }
+
 
 
 };
+
+
+class amg8833Grid {
+
+   constructor(divID, cols = 8, rows = 8) {
+      this.divID = divID;
+      this.cols = cols;
+      this.rows = rows;
+   }
+
+   load(arr) {
+      /* -- -- */
+      let __onrow = function(row) {
+            
+         };
+      /* -- -- */
+      arr.forEach(__onrow);
+   }
+
+
+}
