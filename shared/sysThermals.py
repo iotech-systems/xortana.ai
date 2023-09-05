0ef34720ba055a023247f07f875318c2de2e3ff1
ErikOwsiak <erik@iotech.com>
2023-09-05 12:23:37 +0200

    thermal triggers

--- shared/sysThermals.py
+++ shared/sysThermals.py
@@ -13,8 +13,8 @@ except ModuleNotFoundError:
 finally:
    pass
 
-triggers: {} = {"level0": {48, 34.0}, "level1": {32, 30.0}
-   , "level2": {14, 26.0}}
+triggers: {} = {"level0": {48, 34.0}, "level1": {32, 32.0}
+   , "level2": {16, 30.0}}
 
 
 class sysThermals(object):
