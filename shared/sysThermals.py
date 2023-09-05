e0ebcc55fadf570c29db955ec1d707d08a3b5ff3
ErikOwsiak <erik@iotech.com>
2023-09-05 14:49:48 +0200

    thermal triggers

--- shared/sysThermals.py
+++ shared/sysThermals.py
@@ -147,7 +147,7 @@ class sysThermals(object):
       return key
 
 
-# -- -- -- -- [ testing ] -- -- -- --
+# -- -- -- -- -- [ testing ] -- -- -- -- --
 if __name__ == "__main__":
    obj: sysThermals = sysThermals()
    obj.init()
